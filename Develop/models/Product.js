ff660cee3ab860fbff84b5f8dd33840a38151a7c
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-11-27 19:54:04 -0600

    models and associations

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model { }
@@ -21,6 +22,13 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: id
+      }
     }
   },
   {
